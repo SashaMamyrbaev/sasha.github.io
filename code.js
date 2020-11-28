5e3c5e1bfc24e8f294b548d71c18560c6d5ad3cd
SashaMamyrbaev <71871854+SashaMamyrbaev@users.noreply.github.com>
2020-11-28 20:19:21 +0200

    Add files via upload

---
+++ code.js
@@ -0,0 +1,18 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+$("html, body").animate({
+    scrollTop:0
+  },1000);
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
